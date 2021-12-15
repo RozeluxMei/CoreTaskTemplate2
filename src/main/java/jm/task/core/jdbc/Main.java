70fe8fc2d7a30f153c618bd374b8301cadabe505
Rozelux <rozelux@yandex.ru>
2021-12-15 21:05:31 +0300

    21:04

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // реализуйте алгоритм здесь
+        String url = "jdbc:mysql://localhost:3306/learning";
+        String user = "root";
+        String password = "root";
+
+        Util.connectToMysql(url,user,password);
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static void connectToMysql (String url, String userName, String password) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (Connection connection = DriverManager.getConnection (url, userName, password)){
+            System.out.println("We're connected");
+        }
+    }
 }
